5f624a7a092d269532b302b8c332f8dcbc1d1773
agnes <agnes@AgnesSherlin>
2023-09-06 22:27:52 +0530

    first commit

---
+++ AgnesSherlin/src/staticprogram/staticdemo.java
@@ -0,0 +1,14 @@
+package staticprogram;
+
+public class staticdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(employee.companyName);
+		employee ob = new employee(" Agnes ", 123);
+		System.out.println(ob);
+		employee ob1 = new employee(" Abel ", 785);
+		System.out.println(ob1);
+	}
+
+}
