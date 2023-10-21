2c33865dfdf6356c297d545cc7c3d9c3f87a0b1c
agnes <agnes@AgnesSherlin>
2023-10-21 22:23:24 +0530

    first commit

---
+++ AgnesSherlin/src/threadpro/ExtendThread.java
@@ -0,0 +1,16 @@
+package threadpro;
+
+public class ExtendThread extends Thread{
+	
+	public void run()
+	{
+		System.out.println("Hurray!!..Thread is running.");
+	}
+	public static void main(String[] args) {
+		
+		ExtendThread t = new ExtendThread();
+		t.start(); 	
+		System.out.println("Inside main method");
+		t.run();
+	}
+}
